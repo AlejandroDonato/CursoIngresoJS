acd07172df3d9a9b45b70b21c9164631f9164fd1
AlejandroDonato <musoki88@gmail.com>
2021-07-06 21:11:22 -0300

    ultimo cambio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	nombre = prompt ("ingrese su nombre");
+
+//se pueden poner las dos cosas en la misma linea: let nombre = "texto"
+//let declara la variable y = la inicializa
+//prompt = cuadro para ingresar texto
+
+	alert(nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,16 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+	//reserva de espacio para guardar datos
+
+	nombre = prompt("ingrese texto");
+
+	//guarda en la variable el texto que se coloque en la ventana emergente
+	
+	document.getElementById("txtIdNombre").value = nombre;
+	
+	//carga el texto guardado en la variable en el recuadro de texto del html
+
 }
 
