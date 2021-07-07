0597a826adb81b7a0a1832b136a479b8b9e047aa
AlejandroDonato <musoki88@gmail.com>
2021-07-07 06:21:20 -0300

    ultimo cambio

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,22 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numerouno;
+	let numerodos;
+	let resultado;
+
+	numerouno = parseInt (document.getElementById ("txtIdNumeroUno").value);
+	numerodos = parseInt (document.getElementById ("txtIdNumeroDos").value);
+	
+	//parseInt modifica una cadena de texto (ascii) a valor numerico
+	//parseFloat
+
+	resultado = numerouno + numerodos;
+
+	
+	//alert ("la suma es " resultado);
+	alert (`la suma es ${resultado}`);
+
+
 }
 
